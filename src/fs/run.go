52484e4e7a33e1cc22b61255236253eefcfcc9a5
steden <1470804@qq.com>
2022-07-14 18:26:55 +0800

    增加启动器

---
+++ src/fs/run.go
@@ -0,0 +1,38 @@
+package fs
+
+import (
+	"fmt"
+	"fs/utils/net"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// StartupAt 应用启动时间
+var StartupAt time.Time
+
+// AppName 应用名称
+var AppName string
+
+// AppId 应用ID
+var AppId int64
+
+// AppIp 应用IP
+var AppIp string
+
+func Run(appName string) {
+	rand.Seed(time.Now().UnixNano())
+	StartupAt = time.Now()
+	appidString := strings.Join([]string{strconv.FormatInt(StartupAt.UnixNano()/1e6, 10), strconv.Itoa(rand.Intn(999-100) + 100)}, "")
+	AppId, _ = strconv.ParseInt(appidString, 10, 64)
+	AppIp = net.Ip
+	AppName = appName
+
+	fmt.Println("系统时间：", StartupAt)
+	fmt.Println("进程ID：", os.Getppid())
+	fmt.Println("应用ID：", AppId)
+	fmt.Println("应用IP：", AppIp)
+	fmt.Println("---------------------------------------")
+}
