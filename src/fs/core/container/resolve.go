815efb8c90ee3a82be4d49c0961420c39367e978
steden <1470804@qq.com>
2022-07-21 01:21:44 +0800

    cancel return error

--- src/fs/core/container/resolve.go
+++ src/fs/core/container/resolve.go
@@ -1,7 +1,7 @@
 package container
 
 // Resolve 从容器中获取实例
-func Resolve[T any]() (t T, err error) {
-	err = container.Resolve(&t)
+func Resolve[T any]() (t T) {
+	_ = container.Resolve(&t)
 	return
 }
