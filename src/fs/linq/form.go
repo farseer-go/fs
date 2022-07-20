316d2dd29f8a33b931fcbf8e1c1d82dc3c084683
steden <1470804@qq.com>
2022-07-21 01:19:22 +0800

    linq.form增加ToPageList、Take方法

--- src/fs/linq/form.go
+++ src/fs/linq/form.go
@@ -1,23 +1,17 @@
 package linq
 
 import (
-	"reflect"
+	"fs/core"
 )
 
 type linqForm[T any] struct {
 	// source array
-	source *[]T
-	// array type
-	arrayType reflect.Type
-	// element type
-	elementType reflect.Type
-	// value type
-	value reflect.Value
+	source []T
 }
 
 func From[T any](source []T) linqForm[T] {
 	return linqForm[T]{
-		source: &source,
+		source: source,
 	}
 }
 
@@ -26,18 +20,18 @@ type WhereFunc[T any] func(item T) bool
 // Where 对数据进行筛选
 func (receiver linqForm[T]) Where(fn WhereFunc[T]) linqForm[T] {
 	var lst []T
-	for _, item := range *receiver.source {
+	for _, item := range receiver.source {
 		if fn(item) {
 			lst = append(lst, item)
 		}
 	}
-	receiver.source = &lst
+	receiver.source = lst
 	return receiver
 }
 
 // Find 查找符合条件的元素
 func (receiver linqForm[T]) Find(fn WhereFunc[T]) T {
-	for _, item := range *receiver.source {
+	for _, item := range receiver.source {
 		if fn(item) {
 			return item
 		}
@@ -49,7 +43,7 @@ func (receiver linqForm[T]) Find(fn WhereFunc[T]) T {
 // FindAll 查找符合条件的元素列表
 func (receiver linqForm[T]) FindAll(fn WhereFunc[T]) []T {
 	var lst []T
-	for _, item := range *receiver.source {
+	for _, item := range receiver.source {
 		if fn(item) {
 			lst = append(lst, item)
 		}
@@ -59,8 +53,8 @@ func (receiver linqForm[T]) FindAll(fn WhereFunc[T]) []T {
 
 // First 查找符合条件的元素
 func (receiver linqForm[T]) First() T {
-	if len(*receiver.source) > 0 {
-		return (*receiver.source)[0]
+	if len(receiver.source) > 0 {
+		return (receiver.source)[0]
 	}
 	var t T
 	return t
@@ -68,22 +62,67 @@ func (receiver linqForm[T]) First() T {
 
 // ToArray 查找符合条件的元素列表
 func (receiver linqForm[T]) ToArray() []T {
-	return *receiver.source
+	return receiver.source
 }
 
 // RemoveAll 移除条件=true的元素
 func (receiver linqForm[T]) RemoveAll(fn WhereFunc[T]) []T {
 	var lst []T
-	for _, item := range *receiver.source {
+	for _, item := range receiver.source {
 		if !fn(item) {
 			lst = append(lst, item)
 		}
 	}
-	receiver.source = &lst
+	receiver.source = lst
 	return lst
 }
 
 // Count 获取数量
 func (receiver linqForm[T]) Count() int {
-	return len(*receiver.source)
+	return len(receiver.source)
+}
+
+// ToPageList 数组分页
+func (receiver linqForm[T]) ToPageList(pageSize int, pageIndex int) core.PageList[T] {
+	pageList := core.PageList[T]{
+		RecordCount: int64(len(receiver.source)),
+	}
+
+	if pageSize < 1 {
+		pageSize = 10
+	}
+
+	// 计算总页数
+	var allCurrentPage int64 = 0
+	// 没有设置pageIndex，则按take返回
+	if pageIndex < 1 {
+		pageList.List = receiver.Take(pageSize)
+		return pageList
+	}
+
+	allCurrentPage = pageList.RecordCount / int64(pageSize)
+	if pageList.RecordCount%int64(pageSize) != 0 {
+		allCurrentPage++
+	}
+	if allCurrentPage == 0 {
+		allCurrentPage = 1
+	}
+
+	if int64(pageIndex) > allCurrentPage {
+		pageIndex = int(allCurrentPage)
+	}
+	skipCount := pageSize * (pageIndex - 1)
+	pageList.List = receiver.source[skipCount : skipCount+pageSize]
+	return pageList
+}
+
+// Take 返回前多少条数据
+func (receiver linqForm[T]) Take(count int) []T {
+	recordCount := len(receiver.source)
+	// 总长度比count小，则直接返回全部数据
+	if recordCount < count {
+		return receiver.source
+	}
+
+	return receiver.source[0:count]
 }
--- src/fs/linq/form_test.go
+++ src/fs/linq/form_test.go
@@ -12,3 +12,15 @@ func TestFind(t *testing.T) {
 	}).ToArray()
 	fmt.Println(item)
 }
+
+func TestToPageList(t *testing.T) {
+	lst := []int{1, 2, 3, 4, 5, 6, 7}
+	item := From(lst).ToPageList(3, 2)
+	fmt.Println(item)
+}
+
+func TestTake(t *testing.T) {
+	lst := []int{1, 2, 3, 4, 5, 6, 7}
+	item := From(lst).Take(3)
+	fmt.Println(item)
+}
