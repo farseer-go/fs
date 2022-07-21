6c509480222d6d513db933350d40b237bfb43240
steden <1470804@qq.com>
2022-07-21 22:10:57 +0800

    修改mapper的结构名称

--- src/fs/mapper/mapper.go
+++ src/fs/mapper/mapper.go
@@ -4,15 +4,15 @@ import (
 	"github.com/devfeel/mapper"
 )
 
-// MapperArray 转换结构数组
-func MapperArray[T any](fromSlice any) T {
-	var toSlice T
+// Array 转换结构数组
+func Array[T any](fromSlice any) []T {
+	var toSlice []T
 	_ = mapper.MapperSlice(fromSlice, &toSlice)
 	return toSlice
 }
 
-// AutoMapper 转换
-func AutoMapper[T any](fromObj any) T {
+// Single 转换
+func Single[T any](fromObj any) T {
 	var toObj T
 	_ = mapper.MapperSlice(fromObj, &toObj)
 	return toObj
