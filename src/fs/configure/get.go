5a09e931bd09f17efe31ec92cf4ffdb2344f5b17
steden <1470804@qq.com>
2022-07-24 20:23:29 +0800

    发现配置文件不存在时，只打印错误不报异常

---
+++ src/fs/configure/get.go
@@ -0,0 +1,18 @@
+package configure
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile("./farseer.yaml")
+	err := viper.ReadInConfig() //找到并读取配置文件
+	if err != nil {             // 捕获读取中遇到的error
+		fmt.Errorf("读取配置文件farseer.yaml时发生错误: %w \n", err)
+	}
+}
+
+func GetString(key string) string {
+	return viper.GetString(key)
+}
