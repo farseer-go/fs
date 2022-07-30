ae6fb910f1ab92695de16c1ab6b1ab92a191d0bd
steden <1470804@qq.com>
2022-07-30 23:23:52 +0800

    忽略错误

--- utils/file/write.go
+++ utils/file/write.go
@@ -7,23 +7,23 @@ import (
 
 // WriteString 写入文件
 func WriteString(filePath string, content string) {
-	os.WriteFile(filePath, []byte(content), 0766)
+	_ = os.WriteFile(filePath, []byte(content), 0766)
 }
 
 // AppendString 追加文件
 func AppendString(filePath string, content string) {
 	oldContent := ReadString(filePath)
-	os.WriteFile(filePath, []byte(oldContent+content), 0766)
+	_ = os.WriteFile(filePath, []byte(oldContent+content), 0766)
 }
 
 // AppendLine 换行追加文件
 func AppendLine(filePath string, content string) {
 	oldContent := ReadString(filePath)
-	os.WriteFile(filePath, []byte(oldContent+"\n"+content), 0766)
+	_ = os.WriteFile(filePath, []byte(oldContent+"\n"+content), 0766)
 }
 
 // AppendAllLine 换行追加文件
 func AppendAllLine(filePath string, contents []string) {
 	oldContent := ReadString(filePath)
-	os.WriteFile(filePath, []byte(oldContent+"\n"+strings.Join(contents, "\n")), 0766)
+	_ = os.WriteFile(filePath, []byte(oldContent+"\n"+strings.Join(contents, "\n")), 0766)
 }
