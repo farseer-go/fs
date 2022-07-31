a4e6137d4053fc0e2ed90ebb1fc2ecca6dc875ba
harlen <owner_tao@163.com>
2022-07-31 19:14:41 +0800

    调整redis组件

--- cache/redis/redisClient_test.go
+++ cache/redis/redisClient_test.go
@@ -12,20 +12,20 @@ func TestClient(t *testing.T) {
 	if err == nil {
 		fmt.Printf("设置值:%v\n", "...12312")
 	}
-	get, err := client.Exists("key1")
+	get, err := client.string.Exists("key1")
 	if err != nil {
 		fmt.Printf("错误信息：%v\n", err)
 	} else {
 		fmt.Printf("是否存在：%v\n", get)
 	}
 
-	remove, _ := client.Remove("key1")
+	remove, _ := client.string.Remove("key1")
 	fmt.Printf("是否删除：%v\n", remove)
 
 	s, _ := client.string.Get("key1")
 	fmt.Printf("获取值：%v\n", s)
 
-	get2, err2 := client.Exists("key1")
+	get2, err2 := client.string.Exists("key1")
 	if err != nil {
 		fmt.Printf("错误信息：%v\n", err2)
 	} else {
