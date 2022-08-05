407bef7de1b939ab2ec8a62ec1246b73c5a978dd
steden <1470804@qq.com>
2022-08-05 21:19:00 +0800

    添加转换成core.PageList

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -2,6 +2,7 @@ package mapper
 
 import (
 	"github.com/devfeel/mapper"
+	"github.com/farseernet/farseer.go/core"
 )
 
 // Array 数组转换
@@ -16,4 +17,10 @@ func Single[T any](fromObj any) T {
 	var toObj T
 	_ = mapper.MapperSlice(fromObj, &toObj)
 	return toObj
+}
+
+// PageList 转换成core.PageList
+func PageList[TData any](fromObj any, recordCount int64) core.PageList[TData] {
+	lst := Array[TData](fromObj)
+	return core.NewPageList(lst, recordCount)
 }
