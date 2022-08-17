4eceb510ea4824141f1883ab6b0e2cda19c97b16
steden <1470804@qq.com>
2022-08-18 00:22:54 +0800

    检查是否有初始化

--- core/container/register.go
+++ core/container/register.go
@@ -1,6 +1,9 @@
 package container
 
-import "github.com/studyzy/iocgo"
+import (
+	"github.com/farseer-go/fs/exception"
+	"github.com/studyzy/iocgo"
+)
 
 // Container 容器操作
 var container *iocgo.Container
@@ -11,5 +14,8 @@ func InitContainer() {
 
 // Register 注册接口
 func Register(constructor any) error {
+	if container == nil {
+		exception.ThrowRefuseException("请先调用fs.Initialize[Module]()初始化模块")
+	}
 	return container.Register(constructor)
 }
--- core/container/resolve.go
+++ core/container/resolve.go
@@ -1,7 +1,12 @@
 package container
 
+import "github.com/farseer-go/fs/exception"
+
 // Resolve 从容器中获取实例
 func Resolve[T any]() (t T) {
+	if container == nil {
+		exception.ThrowRefuseException("请先调用fs.Initialize[Module]()初始化模块")
+	}
 	_ = container.Resolve(&t)
 	return
 }
