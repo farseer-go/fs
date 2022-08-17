f420e0884917005d62bbbcb8c23408d242b79fb4
steden <1470804@qq.com>
2022-08-17 15:29:18 +0800

    配置文档错误时，只打印日志，不报错

--- configure/get.go
+++ configure/get.go
@@ -9,13 +9,18 @@ func InitConfigure() {
 	viper.SetConfigFile("../../doc/farseer.yaml")
 	err := viper.ReadInConfig() //找到并读取配置文件
 	if err != nil {             // 捕获读取中遇到的error
-		log.Fatalf("读取配置文件farseer.yaml时发生错误: %s \n", err)
+		log.Printf("读取配置文件farseer.yaml时发生错误: %s \n", err)
 	}
 }
 
 // GetString 获取配置
 func GetString(key string) string {
 	return viper.GetString(key)
+}
+
+// GetSubNodes 获取所有子节点
+func GetSubNodes(key string) map[string]string {
+	return viper.GetStringMapString(key)
 }
 
 // SetDefault 设置配置的默认值
