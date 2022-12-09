c9075d47d836a06f0f1899018c22f39cf3180bfa
steden <1470804@qq.com>
2022-12-09 22:05:54 +0800

    增加types.IsGoBasicType 是否为Go内置基础类型

--- types/getType.go
+++ types/getType.go
@@ -15,6 +15,16 @@ func GetRealType(val reflect.Value) reflect.Type {
 	return val.Type()
 }
 
+func GetRealType2(val reflect.Type) reflect.Type {
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if val.Kind() == reflect.Interface {
+		val = val.Elem()
+	}
+	return val
+}
+
 // GetInParam 获取方法的入参
 func GetInParam(methodType reflect.Type) []reflect.Type {
 	var arr []reflect.Type
@@ -31,13 +41,4 @@ func GetOutParam(methodType reflect.Type) []reflect.Type {
 		arr = append(arr, methodType.Out(inIndex))
 	}
 	return arr
-}
-
-// IsDtoModel 当只有一个参数，且非集合类型，又是结构类型时，判断为DTO
-func IsDtoModel(lst []reflect.Type) bool {
-	if len(lst) != 1 {
-		return false
-	}
-
-	return !IsCollections(lst[0]) && lst[0].Kind() == reflect.Struct
 }
--- types/isType.go
+++ types/isType.go
@@ -41,9 +41,45 @@ func IsCollections(ty reflect.Type) bool {
 }
 
 // IsStruct 是否为Struct
-func IsStruct(val reflect.Value) (reflect.Type, bool) {
-	realType := GetRealType(val)
-	return realType, realType.Kind() == reflect.Struct
+func IsStruct(ty reflect.Type) bool {
+	realType := GetRealType2(ty)
+	if realType.Kind() == reflect.Struct {
+		if IsCollections(realType) || realType.String() == "time.Time" {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+// IsGoBasicType 是否为Go内置基础类型
+func IsGoBasicType(ty reflect.Type) bool {
+	realType := GetRealType2(ty)
+	switch realType.Kind() {
+	case reflect.Bool |
+		reflect.Int |
+		reflect.Int8 |
+		reflect.Int16 |
+		reflect.Int32 |
+		reflect.Int64 |
+		reflect.Uint |
+		reflect.Uint8 |
+		reflect.Uint16 |
+		reflect.Uint32 |
+		reflect.Uint64 |
+		reflect.Float32 |
+		reflect.Float64 |
+		reflect.Complex64 |
+		reflect.Complex128 |
+		reflect.String:
+		return true
+	}
+	switch realType.String() {
+	case "time.Time":
+		return true
+	}
+
+	return false
 }
 
 // IsEsIndexSet 判断类型是否为ES的IndexSet类型
@@ -56,4 +92,13 @@ func IsEsIndexSet(val reflect.Value) (reflect.Type, bool) {
 func IsDataTableSet(val reflect.Value) (reflect.Type, bool) {
 	realType := GetRealType(val)
 	return realType, strings.HasPrefix(realType.String(), "data.TableSet[")
+}
+
+// IsDtoModel 当只有一个参数，且非集合类型，又是结构类型时，判断为DTO
+func IsDtoModel(lst []reflect.Type) bool {
+	if len(lst) != 1 {
+		return false
+	}
+
+	return !IsCollections(lst[0]) && lst[0].Kind() == reflect.Struct
 }
