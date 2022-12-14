b6391c32fd115060c82735b5c5f08e9a2bc8b9db
gelin9527 <1376609520@qq.com>
2022-12-14 20:44:50 +0800

    调整 IsGoBasicType 方法

--- go.mod
+++ go.mod
@@ -4,16 +4,19 @@ go 1.19
 
 require (
 	github.com/spf13/viper v1.12.0
+	github.com/stretchr/testify v1.7.1
 	github.com/studyzy/iocgo v0.0.0-20220102165609-54d474ad4d2a
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
--- types/isType.go
+++ types/isType.go
@@ -56,22 +56,8 @@ func IsStruct(ty reflect.Type) bool {
 func IsGoBasicType(ty reflect.Type) bool {
 	realType := GetRealType2(ty)
 	switch realType.Kind() {
-	case reflect.Bool |
-		reflect.Int |
-		reflect.Int8 |
-		reflect.Int16 |
-		reflect.Int32 |
-		reflect.Int64 |
-		reflect.Uint |
-		reflect.Uint8 |
-		reflect.Uint16 |
-		reflect.Uint32 |
-		reflect.Uint64 |
-		reflect.Float32 |
-		reflect.Float64 |
-		reflect.Complex64 |
-		reflect.Complex128 |
-		reflect.String:
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.String:
 		return true
 	}
 	switch realType.String() {
