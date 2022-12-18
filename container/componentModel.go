08b23f7a8808fc8564fd10b70d0f0a859077404a
steden <1470804@qq.com>
2022-12-18 20:20:47 +0800

    完成注入功能。

--- container/componentModel.go
+++ container/componentModel.go
@@ -7,18 +7,31 @@ import (
 
 // 实现类模型
 type componentModel struct {
-	name            string            // 别名
-	lifecycle       eumLifecycle.Enum // 生命周期
-	interfaceType   reflect.Type      // 继承的接口
-	getInstanceType any               // 函数的接口
-	instance        any               // 实例
+	name          string            // 别名
+	lifecycle     eumLifecycle.Enum // 生命周期
+	interfaceType reflect.Type      // 继承的接口
+	instanceType  reflect.Type      // 函数类型
+	instanceValue reflect.Value     // 函数值
+	instance      any               // 实例
 }
 
-func NewComponentModel(name string, lifecycle eumLifecycle.Enum, interfaceType reflect.Type, getInstanceType any) componentModel {
+func NewComponentModel(name string, lifecycle eumLifecycle.Enum, interfaceType reflect.Type, funcIns any) componentModel {
 	return componentModel{
-		name:            name,
-		lifecycle:       lifecycle,
-		interfaceType:   interfaceType,
-		getInstanceType: getInstanceType,
+		name:          name,
+		lifecycle:     lifecycle,
+		interfaceType: interfaceType,
+		instanceType:  reflect.TypeOf(funcIns),
+		instanceValue: reflect.ValueOf(funcIns),
+	}
+}
+
+func NewComponentModelByInstance(name string, lifecycle eumLifecycle.Enum, interfaceType reflect.Type, instance any) componentModel {
+	return componentModel{
+		name:          name,
+		lifecycle:     lifecycle,
+		interfaceType: interfaceType,
+		instanceType:  reflect.TypeOf(instance),
+		instanceValue: reflect.ValueOf(instance),
+		instance:      instance,
 	}
 }
--- container/container.go
+++ container/container.go
@@ -24,15 +24,15 @@ func NewContainer() *container {
 	}
 }
 
-// 注册实例
+// 注册实例，添加到依赖列表
 func (r *container) addComponent(model componentModel) {
 	componentModels, exists := r.dependency[model.interfaceType]
 	if !exists {
 		r.dependency[model.interfaceType] = []componentModel{model}
 	} else {
 		for index := 0; index < len(componentModels); index++ {
-			if componentModels[index].name == model.name && componentModels[index].getInstanceType == model.getInstanceType {
-				panic(fmt.Sprintf("container：已存在同样的注册对象,interfaceType=%s,name=%s,getInstanceType=%s", model.interfaceType.String(), model.name, reflect.TypeOf(model.getInstanceType).String()))
+			if componentModels[index].name == model.name && componentModels[index].instanceType == model.instanceType {
+				panic(fmt.Sprintf("container：已存在同样的注册对象,interfaceType=%s,name=%s,instanceType=%s", model.interfaceType.String(), model.name, reflect.TypeOf(model.instanceType).String()))
 			}
 		}
 		r.dependency[model.interfaceType] = append(componentModels, model)
@@ -73,26 +73,39 @@ func (r *container) registerInstance(interfaceType any, ins any, name string, li
 		flog.Error("container：实例注册，interfaceType类型只能为Interface")
 		os.Exit(-1)
 	}
-	model := NewComponentModel(name, lifecycle, interfaceTypeOf, ins)
+	model := NewComponentModelByInstance(name, lifecycle, interfaceTypeOf, ins)
 	model.instance = ins
 	r.addComponent(model)
 }
 
 // 获取对象
 func (r *container) resolve(interfaceType reflect.Type, name string) any {
-	componentModels, exists := r.dependency[interfaceType]
-	if !exists {
-		flog.Errorf("container：%s未注册", interfaceType.String())
-		return nil
+	if interfaceType.Kind() == reflect.Pointer {
+		interfaceType = interfaceType.Elem()
 	}
 
-	for i := 0; i < len(componentModels); i++ {
-		// 找到了实现类
-		if componentModels[i].name == name {
-			return r.getOrCreateIns(interfaceType, i)
+	// 通过Interface查找注册过的container
+	if interfaceType.Kind() == reflect.Interface {
+		componentModels, exists := r.dependency[interfaceType]
+		if !exists {
+			flog.Errorf("container：%s未注册", interfaceType.String())
+			return nil
 		}
+
+		for i := 0; i < len(componentModels); i++ {
+			// 找到了实现类
+			if componentModels[i].name == name {
+				return r.getOrCreateIns(interfaceType, i)
+			}
+		}
+		flog.Errorf("container：%s未注册，name=%s", interfaceType.String(), name)
+
+		// 结构对象，直接动态创建
+	} else if interfaceType.Kind() == reflect.Struct {
+		return r.createIns(componentModel{
+			instanceType: interfaceType,
+		})
 	}
-	flog.Errorf("container：%s未注册，name=%s", interfaceType.String(), name)
 	return nil
 }
 
@@ -111,22 +124,25 @@ func (r *container) getOrCreateIns(interfaceType reflect.Type, index int) any {
 
 // 根据类型，动态创建实例
 func (r *container) createIns(model componentModel) any {
-	getInstanceType := reflect.TypeOf(model.getInstanceType)
-
-	if getInstanceType.Kind() == reflect.Func {
+	if model.instanceType.Kind() == reflect.Func {
 		var arr []reflect.Value
 		// 构造函数，需要分别取出入参值
-		for inIndex := 0; inIndex < getInstanceType.NumIn(); inIndex++ {
-			val := reflect.ValueOf(r.resolveDefaultOrFirstComponent(getInstanceType.In(inIndex)))
+		for inIndex := 0; inIndex < model.instanceType.NumIn(); inIndex++ {
+			val := reflect.ValueOf(r.resolveDefaultOrFirstComponent(model.instanceType.In(inIndex)))
 			arr = append(arr, val)
 		}
 		if arr == nil {
 			arr = []reflect.Value{}
 		}
-		return reflect.ValueOf(model.getInstanceType).Call(arr)[0].Interface()
+		return r.inject(model.instanceValue.Call(arr)[0].Interface())
 	}
-	if getInstanceType.Kind() == reflect.Struct {
-		return model.getInstanceType
+
+	if model.instanceType.Kind() == reflect.Struct || model.instanceType.Kind() == reflect.Pointer {
+		if model.instance != nil {
+			return r.inject(model.instance)
+		} else {
+			return r.injectByType(model.instanceType)
+		}
 	}
 	return nil
 }
@@ -148,4 +164,33 @@ func (r *container) resolveDefaultOrFirstComponent(interfaceType reflect.Type) a
 		}
 	}
 	return r.getOrCreateIns(interfaceType, findIndex)
+}
+
+// 解析注入
+func (r *container) inject(ins any) any {
+	if ins == nil {
+		return ins
+	}
+	insVal := reflect.Indirect(reflect.ValueOf(ins))
+	for i := 0; i < insVal.NumField(); i++ {
+		field := insVal.Type().Field(i)
+		if field.IsExported() && field.Type.Kind() == reflect.Interface {
+			fieldIns := r.resolve(field.Type, field.Tag.Get("inject"))
+			insVal.Field(i).Set(reflect.ValueOf(fieldIns))
+		}
+	}
+	return ins
+}
+
+// 解析注入
+func (r *container) injectByType(instanceType reflect.Type) any {
+	instanceVal := reflect.New(instanceType).Elem()
+	for i := 0; i < instanceVal.NumField(); i++ {
+		field := instanceVal.Type().Field(i)
+		if field.IsExported() && field.Type.Kind() == reflect.Interface {
+			fieldIns := r.resolve(field.Type, field.Tag.Get("inject"))
+			instanceVal.Field(i).Set(reflect.ValueOf(fieldIns))
+		}
+	}
+	return instanceVal.Interface()
 }
