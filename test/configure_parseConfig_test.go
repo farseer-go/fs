0d1237f4a438bdaeddb92b85fc62dccd363deb34
steden <1470804@qq.com>
2022-12-26 01:21:00 +0800

    增加单元测试覆盖率

--- test/configure_parseConfig_test.go
+++ test/configure_parseConfig_test.go
@@ -12,12 +12,20 @@ type testConfig struct {
 	PoolMaxSize      int
 	PoolMinSize      int
 	ConnectionString string
+	UsePool          bool
 }
 
 func TestParseConfig(t *testing.T) {
-	conf := "dataType=MySql,poolMaxSize=50,poolMinSize=1,connectionString=root:steden@123@tcp(mysql:3306)/fops?charset=utf8&parseTime=True&loc=Local"
+	conf := "dataType=MySql,poolMaxSize=50,poolMinSize=1,connectionString=root:steden@123@tcp(mysql:3306)/fops?charset=utf8&parseTime=True&loc=Local,UsePool=true"
 	dbConfig := configure.ParseConfig[testConfig](conf)
 	assert.Equal(t, dbConfig.PoolMaxSize, 50)
 	assert.Equal(t, dbConfig.PoolMinSize, 1)
 	assert.Equal(t, dbConfig.DataType, "MySql")
+	assert.Equal(t, dbConfig.UsePool, true)
+
+	assert.Panics(t, func() {
+		configure.ParseConfig[testConfig]("dataType")
+		configure.ParseConfig[string]("")
+	})
+
 }
--- test/configure_test.go
+++ test/configure_test.go
@@ -1,14 +1,16 @@
 package test
 
 import (
+	"github.com/farseer-go/fs"
 	"github.com/farseer-go/fs/configure"
+	"github.com/farseer-go/fs/modules"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
 func TestGetArray(t *testing.T) {
-	_ = configure.ReadInConfig()
+	fs.Initialize[modules.FarseerKernelModule]("unit test")
 	assert.Equal(t, "", configure.GetString("a.b.c"))
 	configure.SetDefault("a.b.c", "123")
 	assert.Equal(t, "123", configure.GetString("a.b.c"))
@@ -20,9 +22,20 @@ func TestGetArray(t *testing.T) {
 	assert.Equal(t, "a1", arr[0])
 }
 
-// 测试环境变量
-func TestEnv(t *testing.T) {
-	_ = configure.ReadInConfig()
+func TestConfigureGet(t *testing.T) {
+	fs.Initialize[modules.FarseerKernelModule]("unit test")
+	arr := configure.GetStrings("Database.default")
+	assert.Equal(t, "DataType=mysql", arr[0])
+	assert.Equal(t, "PoolMaxSize=50", arr[1])
+	assert.Equal(t, "PoolMinSize=1", arr[2])
+
+	assert.Equal(t, 20, configure.GetInt("FSS.ReservedTaskCount"))
+	assert.Equal(t, int64(20), configure.GetInt64("FSS.ReservedTaskCount"))
+	assert.Equal(t, true, configure.GetBool("Log.Component.httpInvoke"))
+
+	assert.Len(t, configure.GetSubNodes("A.B"), 0)
+	assert.Len(t, configure.GetSlice("A.B"), 0)
+
 	assert.Equal(t, "DataType=mysql,PoolMaxSize=50,PoolMinSize=1,ConnectionString=root:steden@123@tcp(192.168.1.8:3306)/fss_demo?charset=utf8&parseTime=True&loc=Local", configure.GetString("Database.default"))
 	os.Setenv("Database_default", "aaa")
 	assert.Equal(t, "aaa", configure.GetString("Database.default"))
