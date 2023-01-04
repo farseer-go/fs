c6b13d8ab39cba866bfe76a7e224187226d8b66c
steden <1470804@qq.com>
2023-01-04 18:23:35 +0800

    增加并发测试

--- test/snowflake_test.go
+++ test/snowflake_test.go
@@ -4,12 +4,29 @@ import (
 	"github.com/farseer-go/fs/snowflake"
 	"runtime"
 	"testing"
+	"time"
 )
 
 func TestSnowflake(t *testing.T) {
 	runtime.GOMAXPROCS(1024)
 	snowflake.Init(8, 12)
-	for i := 0; i < 100000; i++ {
-		snowflake.GenerateId()
-	}
+	go func() {
+		for i := 0; i < 100000; i++ {
+			go snowflake.GenerateId()
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 100000; i++ {
+			go snowflake.GenerateId()
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 100000; i++ {
+			go snowflake.GenerateId()
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
 }
