32b5f9649d0c58a74629d5cf6b97d734bcfac332
steden <1470804@qq.com>
2023-02-08 01:18:56 +0800

    将事件参数移到fs模块

---
+++ core/eventArgs.go
@@ -0,0 +1,15 @@
+package core
+
+// EventArgs 事件属性
+type EventArgs struct {
+	// 唯一标识
+	Id string
+	// 事件的发布时间
+	CreateAt int64
+	// 消息内容
+	Message any
+	// 执行失败次数
+	ErrorCount int
+	// 事件名称
+	EventName string
+}
--- core/iEvent.go
+++ core/iEvent.go
@@ -3,5 +3,5 @@ package core
 // IEvent 定义的一个通用的事件发布接口
 type IEvent interface {
 	// Publish 发布消息
-	Publish(message any)
+	Publish(message any) error
 }
