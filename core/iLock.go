22d3681d2b05142ef1eaa14659cc850c6ddeecd9
steden <1470804@qq.com>
2023-02-07 22:44:37 +0800

    添加锁规范

---
+++ core/iLock.go
@@ -0,0 +1,15 @@
+package core
+
+// ILock 锁
+type ILock interface {
+	// TryLock 尝试加锁
+	TryLock() bool
+	// GetLock 获取锁，直到获取成功
+	GetLock()
+	// ReleaseLock 释放锁
+	ReleaseLock()
+	// TryLockRun 尝试加锁，执行完后，自动释放锁
+	TryLockRun(fn func()) bool
+	// GetLockRun 获取锁，直到获取成功，执行完后，自动释放锁
+	GetLockRun(fn func())
+}
