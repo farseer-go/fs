ed65135063b4e2c9269cf28c1fe672c6791a8656
steden <1470804@qq.com>
2023-07-10 09:34:09 +0800

    合并文件

--- flog/iFormatter.go
+++ flog/iFormatter.go
@@ -1,6 +1,40 @@
 package flog
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/farseer-go/fs/core/eumLogLevel"
+)
+
 // IFormatter 日志格式
 type IFormatter interface {
 	Formatter(log *logData) string
+}
+
+// JsonFormatter json格式输出
+type JsonFormatter struct {
+}
+
+func (r *JsonFormatter) Formatter(log *logData) string {
+	marshal, _ := json.Marshal(log)
+	return string(marshal)
+}
+
+// TextFormatter 文本格式输出
+type TextFormatter struct {
+}
+
+func (r *TextFormatter) Formatter(log *logData) string {
+	var logLevelString string
+	if log.logLevel != eumLogLevel.NoneLevel {
+		logLevelString = Colors[log.logLevel]("[" + log.logLevel.ToString() + "]")
+	} else if log.component != "" {
+		logLevelString = Colors[0]("[" + log.component + "]")
+	}
+
+	if logLevelString != "" {
+		return fmt.Sprintf("%s %s %s", log.createAt.ToString("yyyy-MM-dd hh:mm:ss.ffffff"), logLevelString, log.content)
+	}
+
+	return fmt.Sprintf("%s %s", log.createAt.ToString("yyyy-MM-dd hh:mm:ss.ffffff"), log.content)
 }
--- flog/iLoggerProvider.go
+++ flog/iLoggerProvider.go
@@ -6,3 +6,8 @@ type ILoggerProvider interface {
 	// CreateLogger 创建对应的Logger对象，创建Logger
 	CreateLogger(categoryName string, formatter IFormatter, logLevel eumLogLevel.Enum) ILoggerPersistent
 }
+
+// ILoggerPersistent 实际的日志记录器
+type ILoggerPersistent interface {
+	Log(LogLevel eumLogLevel.Enum, log *logData, exception error)
+}
