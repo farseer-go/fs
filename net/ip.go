39ce63aac13a019f6964da2724a7554f8aa2d85a
steden <1470804@qq.com>
2023-09-23 21:54:49 +0800

    调整：获取IP方式改为，从C类到A类的顺序获取

--- net/ip.go
+++ net/ip.go
@@ -1,15 +1,43 @@
 package net
 
 // 获取本机IP地址
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 var ip string
-var ips []string
+var ipList []string
 
 func GetIp() string {
 	if ip == "" {
-		ips, _ = LocalIPv4s()
-		ip = ips[0]
+		ipList, _ = LocalIPv4s()
+		// 优先使用192.168开头
+		for _, strIp := range ipList {
+			if strings.HasPrefix(strIp, "192.168.") {
+				ip = strIp
+				return ip
+			}
+		}
+
+		// 使用172.20.开头
+		for _, strIp := range ipList {
+			if strings.HasPrefix(strIp, "172.20.") {
+				ip = strIp
+				return ip
+			}
+		}
+
+		// 使用10.开头
+		for _, strIp := range ipList {
+			if strings.HasPrefix(strIp, "10.") {
+				ip = strIp
+				return ip
+			}
+		}
+
+		// 没有192.168.x.x和10.x.x.x开头的，只能取第一个IP返回
+		ip = ipList[0]
 	}
 	return ip
 }
