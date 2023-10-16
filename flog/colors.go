64d118999edfbd7085935c6b2038dfdcdbb628cc
steden <1470804@qq.com>
2023-10-16 11:08:55 +0800

    新增：颜色的替换方法

--- flog/colors.go
+++ flog/colors.go
@@ -3,6 +3,7 @@ package flog
 import (
 	"fmt"
 	"github.com/farseer-go/fs/parse"
+	"strings"
 )
 
 // brush is a color join function
@@ -33,9 +34,35 @@ func Red(text any) string {
 	return "\u001B[1;31m" + parse.Convert(text, "") + "\u001B[0m"
 }
 
+// ReplaceRed 转为红色字体
+func ReplaceRed(strSource string, text string) string {
+	return strings.ReplaceAll(strSource, text, Red(text))
+}
+
+// ReplaceReds 转为红色字体
+func ReplaceReds(strSource string, texts ...string) string {
+	for _, text := range texts {
+		strSource = strings.ReplaceAll(strSource, text, Red(text))
+	}
+	return strSource
+}
+
 // Yellow 转为黄色字体
 func Yellow(text any) string {
 	return "\u001B[1;33m" + parse.Convert(text, "") + "\u001B[0m"
+}
+
+// ReplaceYellow 转为黄色字体
+func ReplaceYellow(strSource string, text string) string {
+	return strings.ReplaceAll(strSource, text, Yellow(text))
+}
+
+// ReplaceYellows 转为黄色字体
+func ReplaceYellows(strSource string, texts ...string) string {
+	for _, text := range texts {
+		strSource = strings.ReplaceAll(strSource, text, Yellow(text))
+	}
+	return strSource
 }
 
 // Green 转为绿色字体
@@ -43,7 +70,33 @@ func Green(text any) string {
 	return "\u001B[1;32m" + parse.Convert(text, "") + "\u001B[0m"
 }
 
+// ReplaceGreen 转为绿色字体
+func ReplaceGreen(strSource string, text string) string {
+	return strings.ReplaceAll(strSource, text, Green(text))
+}
+
+// ReplaceGreens 转为绿色字体
+func ReplaceGreens(strSource string, texts ...string) string {
+	for _, text := range texts {
+		strSource = strings.ReplaceAll(strSource, text, Green(text))
+	}
+	return strSource
+}
+
 // Blue 转为蓝色字体
 func Blue(text any) string {
 	return "\u001B[1;34m" + parse.Convert(text, "") + "\u001B[0m"
+}
+
+// ReplaceBlue 转为蓝色字体
+func ReplaceBlue(strSource string, text string) string {
+	return strings.ReplaceAll(strSource, text, Blue(text))
+}
+
+// ReplaceBlues 转为蓝色字体
+func ReplaceBlues(strSource string, texts ...string) string {
+	for _, text := range texts {
+		strSource = strings.ReplaceAll(strSource, text, Blue(text))
+	}
+	return strSource
 }
