68c77bbc42f39a877aa63ae673320eaa9458d71d
steden <1470804@qq.com>
2023-10-19 21:16:12 +0800

    新增：asyncLocal类型，用于实现routine的泛型版本

---
+++ asyncLocal/newAsyncLocal.go
@@ -0,0 +1,36 @@
+package asyncLocal
+
+import (
+	"github.com/timandy/routine"
+)
+
+type AsyncLocal[T any] struct {
+	threadLocal routine.ThreadLocal
+}
+
+// New 创建一个AsyncLocal
+func New[T any]() AsyncLocal[T] {
+	return AsyncLocal[T]{
+		threadLocal: routine.NewInheritableThreadLocal(),
+	}
+}
+
+// Get 获取值
+func (receiver AsyncLocal[T]) Get() T {
+	val := receiver.threadLocal.Get()
+	if val == nil {
+		var t T
+		return t
+	}
+	return val.(T)
+}
+
+// Set 设置值
+func (receiver AsyncLocal[T]) Set(t T) {
+	receiver.threadLocal.Set(t)
+}
+
+// Remove 移除对象
+func (receiver AsyncLocal[T]) Remove() {
+	receiver.threadLocal.Remove()
+}
