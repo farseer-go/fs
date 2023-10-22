4c8d139b79e41856f2688400d4feb228ced31016
steden <1470804@qq.com>
2023-10-23 00:48:39 +0800

    调整：链路追踪异常时，记录调用者信息

--- trace/baseTraceDetail.go
+++ trace/baseTraceDetail.go
@@ -2,8 +2,12 @@ package trace
 
 import (
 	"github.com/farseer-go/collections"
+	"github.com/farseer-go/fs"
 	"github.com/farseer-go/fs/asyncLocal"
+	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/linkTrace/eumCallType"
+	"runtime"
+	"strings"
 	"time"
 )
 
@@ -15,7 +19,7 @@ type BaseTraceDetail struct {
 	DetailId         int64            // 明细ID
 	ParentDetailId   int64            // 父级明细ID
 	Level            int              // 当前层级（入口为0层）
-	CallMethod       string           // 调用方法
+	MethodName       string           // 调用方法
 	CallType         eumCallType.Enum // 调用类型
 	Timeline         time.Duration    // 从入口开始统计
 	UnTraceTs        time.Duration    // 上一次结束到现在开始之间未Trace的时间
@@ -25,6 +29,9 @@ type BaseTraceDetail struct {
 	IsException      bool             // 是否执行异常
 	ExceptionMessage string           // 异常信息
 	ignore           bool             // 忽略这次的链路追踪
+	CallFile         string           // 调用者文件路径
+	CallLine         int              // 调用者行号
+	CallFuncName     string           // 调用者函数名称
 }
 
 func (receiver *BaseTraceDetail) SetSql(DbName string, tableName string, sql string) {}
@@ -37,6 +44,8 @@ func (receiver *BaseTraceDetail) End(err error) {
 	if err != nil {
 		receiver.IsException = true
 		receiver.ExceptionMessage = err.Error()
+		// 调用者
+		receiver.CallFile, receiver.CallFuncName, receiver.CallLine = getCallerInfo()
 	}
 
 	// 移除层级
@@ -55,4 +64,30 @@ func (receiver *BaseTraceDetail) IsIgnore() bool {
 }
 func (receiver *BaseTraceDetail) GetLevel() int {
 	return receiver.Level
+}
+
+func getCallerInfo() (string, string, int) {
+	// 获取调用栈信息
+	pc := make([]uintptr, 15) // 假设最多获取 10 层调用栈
+	n := runtime.Callers(4, pc)
+	frames := runtime.CallersFrames(pc[:n])
+
+	// 遍历调用栈帧
+	for {
+		frame, more := frames.Next()
+		if !strings.HasSuffix(frame.File, "_test.go") && (!flog.IsSysCom(frame.File) || strings.HasSuffix(frame.File, "healthCheck.go")) { // !strings.HasPrefix(file, gormSourceDir) ||
+			// 移除绝对路径
+			prefixFunc := frame.Function[0 : strings.Index(frame.Function, fs.PathSymbol)+1]
+			packageIndex := strings.Index(frame.File, prefixFunc)
+			file := frame.File[packageIndex:]
+
+			// 只要最后的方法名
+			funcName := frame.Function[strings.LastIndex(frame.Function, fs.PathSymbol)+1:] + "()"
+			return file, funcName, frame.Line
+		}
+		if !more {
+			break
+		}
+	}
+	return "", "", 0
 }
