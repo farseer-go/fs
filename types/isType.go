d0d975a329c594cf60932fa4ac2f1bb76073cc56
steden <1470804@qq.com>
2023-11-19 17:31:45 +0800

    新增：GroupBy方法支持map[x]collections.List[x]类型

--- types/isType.go
+++ types/isType.go
@@ -23,6 +23,11 @@ func IsList(val reflect.Value) (reflect.Type, bool) {
 	return realType, strings.HasPrefix(realType.String(), "collections.List[")
 }
 
+// IsList 判断类型是否为List
+func IsListByType(realType reflect.Type) (reflect.Type, bool) {
+	return realType, strings.HasPrefix(realType.String(), "collections.List[")
+}
+
 // IsDictionary 判断类型是否为Dictionary
 func IsDictionary(val reflect.Value) (reflect.Type, bool) {
 	realType := GetRealType(val)
