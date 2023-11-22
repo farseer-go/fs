faa1940a9c1ccba405704073d8991b39767456f8
steden <1470804@qq.com>
2023-11-22 17:22:50 +0800

    更新依赖

--- system/resource.go
+++ system/resource.go
@@ -3,10 +3,10 @@ package system
 import (
 	"fmt"
 	"github.com/farseer-go/fs/net"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 type resource struct {
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+go get github.com/shirou/gopsutil/v3
 go get github.com/timandy/routine
 go get github.com/stretchr/testify
 go get gopkg.in/yaml.v3
