0df0219d37188e3fc1f1f997d90d83ae532dac2b
steden <1470804@qq.com>
2023-12-12 14:18:56 +0800

    新增：GetPageList获取collections.PageList的元素

--- types/list.go
+++ types/list.go
@@ -43,3 +43,14 @@ func ListToArray(lstValue reflect.Value) []any {
 	}
 	return items
 }
+
+// GetPageList 获取collections.PageList的元素
+func GetPageList(pageList any) (any, int64) {
+	pageListValueOf := reflect.ValueOf(pageList)
+	if _, success := IsPageList(pageListValueOf); !success {
+		panic("ToPageList的入参必须是collections.PageList类型")
+	}
+	listValueOf := pageListValueOf.FieldByName("List")
+	recordCountValueOf := pageListValueOf.FieldByName("RecordCount")
+	return listValueOf.Interface(), recordCountValueOf.Interface().(int64)
+}
