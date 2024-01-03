5eb55eddfc779d798727d6949967bb6a35a2e030
steden <1470804@qq.com>
2024-01-04 00:49:16 +0800

    新增：ThrowRefuseExceptionBool、ThrowRefuseExceptionfBool、ThrowRefuseExceptionError 便捷方法

--- exception/refuseException.go
+++ exception/refuseException.go
@@ -16,3 +16,24 @@ func ThrowRefuseException(err string) {
 func ThrowRefuseExceptionf(format string, a ...any) {
 	panic(RefuseException{Message: fmt.Sprintf(format, a...)})
 }
+
+// ThrowRefuseExceptionBool 抛出RefuseException异常
+func ThrowRefuseExceptionBool(isTrue bool, err string) {
+	if isTrue {
+		panic(RefuseException{Message: err})
+	}
+}
+
+// ThrowRefuseExceptionfBool 抛出RefuseException异常
+func ThrowRefuseExceptionfBool(isTrue bool, format string, a ...any) {
+	if isTrue {
+		panic(RefuseException{Message: fmt.Sprintf(format, a...)})
+	}
+}
+
+// ThrowRefuseExceptionError 抛出RefuseException异常
+func ThrowRefuseExceptionError(err error) {
+	if err != nil {
+		panic(RefuseException{Message: err.Error()})
+	}
+}
