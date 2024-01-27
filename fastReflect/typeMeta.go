2bc38354b10940914c7684a4fac045c43fe05106
steden <1470804@qq.com>
2024-01-27 17:40:01 +0800

    移除未使用的字段

--- fastReflect/typeMeta.go
+++ fastReflect/typeMeta.go
@@ -8,32 +8,29 @@ import (
 
 // TypeMeta 类型元数据
 type TypeMeta struct {
-	Name               string              // 字段名称
-	ReflectType        reflect.Type        // 字段类型
-	ReflectTypeString  string              // 类型
-	ReflectStructField reflect.StructField // 字段类型
-	Type               FieldType           // 集合类型
-	IsAnonymous        bool                // 是否为内嵌类型
-	IsExported         bool                // 是否为可导出类型
-	IsIgnore           bool                // 是否为忽略字段
-	IsAddr             bool                // 原类型是否带指针
-	MapKey             reflect.Value       // map key
-	NumField           int                 // 结构体的字段数量
-	ItemMeta           *TypeMeta           // Item元素的Type
-	SliceType          reflect.Type        // ItemType转成切片类型
-	ZeroValue          any                 // 零值时的值
-	Kind               reflect.Kind
-	IsNumber           bool    // 是否为数字
-	IsEmum             bool    // 是否枚举
-	IsString           bool    // 是否为字符串
-	IsBool             bool    // 是否bool
-	IsTime             bool    // 是否time.Time
-	IsDateTime         bool    // 是否dateTime.DateTime
-	IsSliceOrArray     bool    // 是否切片或数组类型
-	IsStruct           bool    // 是否结构体
-	HashCode           uint32  // 每个类型的HashCode都是唯一的
-	Size               uintptr // 内存占用大小
-	TypeIdentity       string  // 类型标识
+	ReflectType       reflect.Type // 字段类型
+	ReflectTypeString string       // 类型
+	Type              FieldType    // 集合类型
+	IsAnonymous       bool         // 是否为内嵌类型
+	IsExported        bool         // 是否为可导出类型
+	IsIgnore          bool         // 是否为忽略字段
+	IsAddr            bool         // 原类型是否带指针
+	NumField          int          // 结构体的字段数量
+	ItemMeta          *TypeMeta    // Item元素的Type
+	SliceType         reflect.Type // ItemType转成切片类型
+	ZeroValue         any          // 零值时的值
+	Kind              reflect.Kind
+	IsNumber          bool    // 是否为数字
+	IsEmum            bool    // 是否枚举
+	IsString          bool    // 是否为字符串
+	IsBool            bool    // 是否bool
+	IsTime            bool    // 是否time.Time
+	IsDateTime        bool    // 是否dateTime.DateTime
+	IsSliceOrArray    bool    // 是否切片或数组类型
+	IsStruct          bool    // 是否结构体
+	HashCode          uint32  // 每个类型的HashCode都是唯一的
+	Size              uintptr // 内存占用大小
+	TypeIdentity      string  // 类型标识
 }
 
 func typeOf(inf *EmptyInterface, reflectType reflect.Type) *TypeMeta {
@@ -45,11 +42,8 @@ func typeOf(inf *EmptyInterface, reflectType reflect.Type) *TypeMeta {
 		Kind:        kind,
 
 		// 作为字段时才能获取
-		IsAnonymous: false,
-		IsExported:  false,
-		IsIgnore:    false,
-		HashCode:    inf.Typ.hash,
-		Size:        inf.Typ.size,
+		HashCode: inf.Typ.hash,
+		Size:     inf.Typ.size,
 
 		ZeroValue: reflect.New(reflectType).Elem().Interface(),
 	}
