925deecd5b7d8b58bd05b19664d5bce3926004a7
steden <1470804@qq.com>
2024-07-15 13:47:37 +0800

    调整：asyncLocal自动释放功能

--- asyncLocal/newAsyncLocal.go
+++ asyncLocal/newAsyncLocal.go
@@ -8,15 +8,23 @@ type AsyncLocal[T any] struct {
 	threadLocal routine.ThreadLocal
 }
 
+// 将同一个协程所New的AsyncLocal放到同一个数组中，在使用完后释放
+var list = make(map[int64][]routine.ThreadLocal)
+
 // New 创建一个AsyncLocal
 func New[T any]() AsyncLocal[T] {
 	// 加入到list集合，用于手动GC
-	threadLocal := routine.NewInheritableThreadLocal()
-	list = append(list, threadLocal)
-
-	return AsyncLocal[T]{
-		threadLocal: threadLocal,
+	al := AsyncLocal[T]{
+		threadLocal: routine.NewInheritableThreadLocal(),
 	}
+	al.AddRelease()
+	return al
+}
+
+// AddRelease 将同一个协程所New的AsyncLocal放到同一个数组中，在使用完后自动释放
+func (receiver AsyncLocal[T]) AddRelease() {
+	goId := routine.Goid()
+	list[goId] = append(list[goId], receiver.threadLocal)
 }
 
 // Get 获取值
@@ -39,10 +47,11 @@ func (receiver AsyncLocal[T]) Remove() {
 	receiver.threadLocal.Remove()
 }
 
-var list []routine.ThreadLocal
-
-func GC() {
-	for _, threadLocal := range list {
+// Release 释放当前使用的AsyncLocal
+func Release() {
+	goId := routine.Goid()
+	for _, threadLocal := range list[goId] {
 		threadLocal.Remove()
 	}
+	delete(list, goId)
 }
