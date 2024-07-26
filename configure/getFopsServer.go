d8940cea54e0ed0d20e6fd3b150a57d036635470
steden <1470804@qq.com>
2024-07-26 18:17:33 +0800

    format

--- configure/getFopsServer.go
+++ configure/getFopsServer.go
@@ -13,12 +13,13 @@ func GetFopsServer() string {
 		return fopsServer
 	}
 
-	fopsServer = strings.ToLower(GetString("Fops.Server"))
-	if !strings.HasPrefix(fopsServer, "http") {
-		panic("[farseer.yaml]Fops.Server配置不正确，示例：https://fops.fsgit.com")
-	}
-	if !strings.HasSuffix(fopsServer, "/") {
-		fopsServer += "/"
+	if fopsServer = strings.ToLower(GetString("Fops.Server")); fopsServer != "" {
+		if !strings.HasPrefix(fopsServer, "http") {
+			panic("[farseer.yaml]Fops.Server配置不正确，示例：https://fops.fsgit.com")
+		}
+		if !strings.HasSuffix(fopsServer, "/") {
+			fopsServer += "/"
+		}
 	}
 	return fopsServer
 }
