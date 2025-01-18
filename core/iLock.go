f0d0dd88b72b9bae961e5fc21b90edf69c1ca635
steden <1470804@qq.com>
2025-01-18 23:36:21 +0800

    修改注释

--- core/iLock.go
+++ core/iLock.go
@@ -8,7 +8,7 @@ type ILock interface {
 	GetLock()
 	// ReleaseLock 释放锁
 	ReleaseLock()
-	// TryLockRun 尝试加锁，执行完后，自动释放锁
+	// TryLockRun 尝试加锁，执行完后，自动释放锁（未获取到时直接退出）
 	TryLockRun(fn func()) bool
 	// GetLockRun 获取锁，直到获取成功，执行完后，自动释放锁
 	GetLockRun(fn func())
