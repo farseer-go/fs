ae8b7880c69a6bf3690bdb46d1e709a574be7203
steden <1470804@qq.com>
2025-03-04 11:00:12 +0800

    测试sonic

--- go.mod
+++ go.mod
@@ -11,12 +11,14 @@ require (
 )
 
 require (
-	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/bytedance/sonic v1.12.10 // indirect
+	github.com/bytedance/sonic/loader v0.2.3 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	golang.org/x/arch v0.12.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/arch v0.14.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
--- snc/json.go
+++ snc/json.go
@@ -1,22 +1,41 @@
 package snc
 
 import (
-	"bytes"
-	"encoding/json"
+	"github.com/bytedance/sonic"
+	"github.com/bytedance/sonic/option"
 )
+
+var snc sonic.API
+
+func init() {
+	// 设置较小的缓冲区，以减少内存使用
+	option.LimitBufferSize = 2 * 1024
+	option.DefaultDecoderBufferSize = 2 * 1024
+	option.DefaultEncoderBufferSize = 2 * 1024
+	option.DefaultAstBufferSize = 2 * 1024
+
+	snc = sonic.Config{
+		CompactMarshaler: true,
+		UseNumber:        true,
+		CopyString:       true,
+	}.Froze()
+}
 
 // 将json转换成对象（反序列化）
 func Unmarshal(data []byte, v any) error {
-	d := json.NewDecoder(bytes.NewReader(data))
-	d.UseNumber()
-	return d.Decode(v)
+	// d := json.NewDecoder(bytes.NewReader(data))
+	// d.UseNumber()
+	// return d.Decode(v)
+	return snc.Unmarshal(data, v)
 }
 
 // 将对象转换成json（序列化）
 func Marshal(val any) ([]byte, error) {
-	return json.Marshal(val)
+	//return json.Marshal(val)
+	return snc.Marshal(val)
 }
 
 func MarshalIndent(v any, prefix, indent string) ([]byte, error) {
-	return json.MarshalIndent(v, prefix, indent)
+	//return json.MarshalIndent(v, prefix, indent)
+	return snc.MarshalIndent(v, prefix, indent)
 }
