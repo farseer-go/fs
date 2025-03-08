255ba35165ba4e30efb0e59a119e20f0f6fb7800
steden <1470804@qq.com>
2025-03-08 18:44:23 +0800

    增加debug日志

--- exception/webException.go
+++ exception/webException.go
@@ -1,6 +1,10 @@
 package exception
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/farseer-go/fs/flog"
+)
 
 type WebException struct {
 	// 异常信息
@@ -10,17 +14,21 @@ type WebException struct {
 
 // ThrowWebException 抛出WebException异常
 func ThrowWebException(statusCode int, err string) {
+	flog.Debugf("WebException:%s", err)
 	panic(WebException{StatusCode: statusCode, Message: err})
 }
 
 // ThrowWebExceptionf 抛出WebException异常
 func ThrowWebExceptionf(statusCode int, format string, a ...any) {
-	panic(WebException{StatusCode: statusCode, Message: fmt.Sprintf(format, a...)})
+	err := fmt.Sprintf(format, a...)
+	flog.Debugf("WebException:%s", err)
+	panic(WebException{StatusCode: statusCode, Message: err})
 }
 
 // ThrowWebExceptionBool 抛出WebException异常
 func ThrowWebExceptionBool(isTrue bool, statusCode int, err string) {
 	if isTrue {
+		flog.Debugf("WebException:%s", err)
 		panic(WebException{StatusCode: statusCode, Message: err})
 	}
 }
@@ -28,13 +36,16 @@ func ThrowWebExceptionBool(isTrue bool, statusCode int, err string) {
 // ThrowWebExceptionfBool 抛出WebException异常
 func ThrowWebExceptionfBool(isTrue bool, statusCode int, format string, a ...any) {
 	if isTrue {
-		panic(WebException{StatusCode: statusCode, Message: fmt.Sprintf(format, a...)})
+		err := fmt.Sprintf(format, a...)
+		flog.Debugf("WebException:%s", err)
+		panic(WebException{StatusCode: statusCode, Message: err})
 	}
 }
 
 // ThrowWebExceptionError 抛出WebException异常
 func ThrowWebExceptionError(statusCode int, err error) {
 	if err != nil {
+		flog.Debugf("WebException:%v", err)
 		panic(WebException{StatusCode: statusCode, Message: err.Error()})
 	}
 }
