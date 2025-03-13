3f1036ffb96df314696864f4db89824205033497
steden <1470804@qq.com>
2025-03-13 17:30:21 +0800

    使用json-iterator

--- go.mod
+++ go.mod
@@ -1,24 +1,19 @@
 module github.com/farseer-go/fs
 
-go 1.21
+go 1.22
 
 toolchain go1.23.3
 
 require (
-	github.com/stretchr/testify v1.9.0
-	github.com/timandy/routine v1.1.4
+	github.com/json-iterator/go v1.1.12
+	github.com/stretchr/testify v1.10.0
+	github.com/timandy/routine v1.1.5
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/bytedance/sonic v1.12.10 // indirect
-	github.com/bytedance/sonic/loader v0.2.3 // indirect
-	github.com/cloudwego/base64x v0.1.5 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	golang.org/x/arch v0.14.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
 )
--- snc/json.go
+++ snc/json.go
@@ -1,26 +1,27 @@
 package snc
 
 import (
-	"encoding/json"
+	// "github.com/bytedance/sonic"
+	// "github.com/bytedance/sonic/option"
+	"bytes"
 
-	"github.com/bytedance/sonic"
-	"github.com/bytedance/sonic/option"
+	jsoniter "github.com/json-iterator/go"
 )
 
-var snc sonic.API
+//var snc sonic.API
 
 func init() {
 	// 设置较小的缓冲区，以减少内存使用
-	option.LimitBufferSize = 2 * 1024
-	option.DefaultDecoderBufferSize = 2 * 1024
-	option.DefaultEncoderBufferSize = 2 * 1024
-	option.DefaultAstBufferSize = 2 * 1024
+	// option.LimitBufferSize = 2 * 1024
+	// option.DefaultDecoderBufferSize = 2 * 1024
+	// option.DefaultEncoderBufferSize = 2 * 1024
+	// option.DefaultAstBufferSize = 2 * 1024
 
-	snc = sonic.Config{
-		CompactMarshaler: true,
-		UseNumber:        true,
-		CopyString:       true,
-	}.Froze()
+	// snc = sonic.Config{
+	// 	CompactMarshaler: true,
+	// 	UseNumber:        true,
+	// 	CopyString:       true,
+	// }.Froze()
 }
 
 // 将json转换成对象（反序列化）
@@ -28,14 +29,17 @@ func Unmarshal(data []byte, v any) error {
 	// d := json.NewDecoder(bytes.NewReader(data))
 	// d.UseNumber()
 	// return d.Decode(v)
-	return snc.Unmarshal(data, v)
+	//return snc.Unmarshal(data, v)
+	d := jsoniter.NewDecoder(bytes.NewReader(data))
+	d.UseNumber()
+	return d.Decode(v)
 }
 
 // 将对象转换成json（序列化）
 func Marshal(val any) ([]byte, error) {
-	return json.Marshal(val)
+	return jsoniter.Marshal(val)
 }
 
 func MarshalIndent(v any, prefix, indent string) ([]byte, error) {
-	return json.MarshalIndent(v, prefix, indent)
+	return jsoniter.MarshalIndent(v, prefix, indent)
 }
