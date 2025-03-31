9234cab668d1f61627e4ae0e5d911957d766f30f
steden <1470804@qq.com>
2025-03-31 17:43:22 +0800

    调整日志

--- flog/printMemery.go
+++ flog/printMemery.go
@@ -5,10 +5,10 @@ import (
 )
 
 // 测试使用
-func PrintMemery() {
+func PrintMemery(title string) {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
-	Infof("===== 内存使用统计 =====")
+	Infof("===== 内存统计（%s） =====", title)
 	Infof("当前分配，还未释放: %.2f MB", float64(memStats.Alloc)/1024/1024)
 	Infof("累计分配（含释放）: %.2f MB", float64(memStats.TotalAlloc)/1024/1024)
 	Infof("系统内存: %.2f MB", float64(memStats.Sys)/1024/1024)
