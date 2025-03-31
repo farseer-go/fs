8b9d3c77b19b16d365c7de2b972a6b1c13bdc345
steden <1470804@qq.com>
2025-03-31 20:03:05 +0800

    日志

--- flog/printMemery.go
+++ flog/printMemery.go
@@ -11,7 +11,6 @@ func PrintMemery(title string) {
 	Infof("===== 内存统计（%s） =====", title)
 	Infof("当前分配，还未释放: %.2f MB", float64(memStats.Alloc)/1024/1024)
 	Infof("累计分配（含释放）: %.2f MB", float64(memStats.TotalAlloc)/1024/1024)
-	Infof("系统内存: %.2f MB", float64(memStats.Sys)/1024/1024)
-	Infof("堆内存，还未释放: %.2f MB", float64(memStats.HeapAlloc)/1024/1024)
+	Infof("向系统申请内存空间: %.2f MB", float64(memStats.Sys)/1024/1024)
 	Infof("GC 次数 : %d", memStats.NumGC)
 }
