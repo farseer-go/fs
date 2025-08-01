3c421e5c72f242806793dc32ba0b94f5ca30d839
steden <1470804@qq.com>
2025-08-01 12:37:05 +0800

    增加协程锁

--- asyncLocal/context.go
+++ asyncLocal/context.go
@@ -9,6 +9,20 @@ import (
 // 在一次请求中共享数据（适用于多层架构中不同层之间的数据共享，省去传值）
 var routineContext AsyncLocal[*sync.Map] = New[*sync.Map]()
 
+// InitContext 初始化同一协程上下文，避免在同一协程中多次初始化
+func InitContext() *sync.Map {
+	mVal := routineContext.Get()
+	if mVal == nil {
+		mVal = &sync.Map{}
+		// 协程锁
+		// 这里使用sync.Mutex是为了在同一协程中可以共享数据，避免多次初始化
+		// 如果需要在不同协程中共享数据，可以使用sync.RWMutex或其他同步机制
+		mVal.Store("ContextLock", &sync.Mutex{})
+		routineContext.Set(mVal)
+	}
+	return mVal
+}
+
 // GetContext 获取Context
 func GetContext[T any](key string) T {
 	var t T
@@ -24,15 +38,32 @@ func GetContext[T any](key string) T {
 	return t
 }
 
+func getLock(mVal *sync.Map) *sync.Mutex {
+	var lock *sync.Mutex
+	if val, exists := mVal.Load("ContextLock"); exists {
+		lock = val.(*sync.Mutex)
+	} else {
+		lock = &sync.Mutex{}
+	}
+	return lock
+}
+
 // GetOrSetContext 获取Context
 func GetOrSetContext[T any](key string, getValFunc func() T) T {
 	var t T
 	key = fastReflect.PointerOf(t).ReflectTypeString + "_" + key
 
-	mVal := routineContext.Get()
-	if mVal == nil {
-		mVal = &sync.Map{}
+	mVal := InitContext()
+	if val, exists := mVal.Load(key); exists {
+		return val.(T)
 	}
+
+	// 使用锁来确保在单个协程环境下的线程安全
+	lock := getLock(mVal)
+	lock.Lock()
+	defer lock.Unlock()
+
+	// 再次检查，避免在锁内重复设置
 	if val, exists := mVal.Load(key); exists {
 		return val.(T)
 	}
