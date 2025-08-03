c40b5f2bfad9dd73d3b5f6ccd6e05b5f0c141603
steden <1470804@qq.com>
2025-08-03 17:56:04 +0800

    缩减SQL

--- trace/traceDetailDatabase.go
+++ trace/traceDetailDatabase.go
@@ -14,7 +14,4 @@ func (receiver *TraceDetailDatabase) SetSql(connectionString string, DbName stri
 	receiver.DbTableName = tableName
 	receiver.DbSql = sql
 	receiver.DbRowsAffected = rowsAffected
-	if len(receiver.DbSql) > 1000 {
-		receiver.DbSql = receiver.DbSql[:1000] + "......"
-	}
 }
