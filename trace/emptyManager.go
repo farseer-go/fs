82dd7dc443d6183e52c763fb3fb6be58fa459b92
steden <steden@fsgit.com>
2026-04-26 16:42:52 +0800

    format

--- trace/emptyManager.go
+++ trace/emptyManager.go
@@ -1,5 +1,7 @@
 package trace
 
+import "github.com/farseer-go/fs/dateTime"
+
 type EmptyManager struct {
 }
 
@@ -42,27 +44,161 @@ func (*EmptyManager) GetCurTrace() *TraceContext { return nil }
 func (*EmptyManager) GetTraceId() string {
 	return ""
 }
-func (*EmptyManager) TraceDatabase() *TraceDetail { return &TraceDetail{} }
+func (*EmptyManager) TraceDatabase() *TraceDetail {
+	return &TraceDetail{
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
+}
 func (*EmptyManager) TraceDatabaseOpen(dbName string, connectString string) *TraceDetail {
-	return &TraceDetail{}
+	return &TraceDetail{
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
 }
 func (*EmptyManager) TraceElasticsearch(method string, IndexName string, AliasesName string) *TraceDetail {
-	return &TraceDetail{}
+	return &TraceDetail{
+		MethodName:          method,
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
 }
 func (*EmptyManager) TraceEtcd(method string, key string, leaseID int64) *TraceDetail {
-	return &TraceDetail{}
+	return &TraceDetail{
+		MethodName:          method,
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
 }
-func (*EmptyManager) TraceHand(name string) *TraceDetail              { return &TraceDetail{} }
-func (*EmptyManager) TraceEventPublish(eventName string) *TraceDetail { return &TraceDetail{} }
+func (*EmptyManager) TraceHand(name string) *TraceDetail {
+	return &TraceDetail{
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
+}
+func (*EmptyManager) TraceEventPublish(eventName string) *TraceDetail {
+	return &TraceDetail{
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
+}
 func (*EmptyManager) TraceMqSend(method string, server string, exchange string, routingKey string) *TraceDetail {
-	return &TraceDetail{}
+	return &TraceDetail{
+		MethodName:          method,
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
 }
 func (*EmptyManager) TraceMq(method string, server string, exchange string) *TraceDetail {
-	return &TraceDetail{}
+	return &TraceDetail{
+		MethodName:          method,
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
 }
 func (*EmptyManager) TraceRedis(method string, key string, field string) *TraceDetail {
-	return &TraceDetail{}
+	return &TraceDetail{
+		MethodName:          method,
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
 }
-func (*EmptyManager) TraceHttp(method string, url string) *TraceDetail { return &TraceDetail{} }
+func (*EmptyManager) TraceHttp(method string, url string) *TraceDetail {
+	return &TraceDetail{
+		MethodName:          method,
+		Exception:           &ExceptionStack{},
+		CreateAt:            dateTime.Now(),
+		TraceDetailHand:     &TraceDetailHand{},
+		TraceDetailDatabase: &TraceDetailDatabase{},
+		TraceDetailEs:       &TraceDetailEs{},
+		TraceDetailEtcd:     &TraceDetailEtcd{},
+		TraceDetailEvent:    &TraceDetailEvent{},
+		TraceDetailGrpc:     &TraceDetailGrpc{},
+		TraceDetailHttp:     &TraceDetailHttp{},
+		TraceDetailMq:       &TraceDetailMq{},
+		TraceDetailRedis:    &TraceDetailRedis{},
+	}
+}
 
 func (*EmptyManager) Push(traceContext *TraceContext, err error) {}
