8111bb6730524480ed7f18795dd2e4f92aae39b2
steden <steden@fsgit.com>
2026-05-10 23:31:14 +0800

    增加应用版本号

--- core/appInfo.go
+++ core/appInfo.go
@@ -5,11 +5,12 @@ import (
 )
 
 var (
-	StartupAt dateTime.DateTime             // 应用启动时间
-	AppName   string                        // 应用名称
-	HostName  string                        // 主机名称
-	AppId     int64                         // 应用ID
-	AppIp     string                        // 应用IP
-	ProcessId int                           // 进程Id
-	Version   string            = "v0.17.2" // 版本号
+	StartupAt  dateTime.DateTime             // 应用启动时间
+	AppName    string                        // 应用名称
+	HostName   string                        // 主机名称
+	AppId      int64                         // 应用ID
+	AppIp      string                        // 应用IP
+	ProcessId  int                           // 进程Id
+	Version    string            = "v0.17.2" // 版本号
+	AppVersion string            = ""        // 应用版本号
 )
